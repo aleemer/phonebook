13d280ffc429a8c4918a01b529426891aa8981e7
aleemer <aleemertariq@gmail.com>
2024-02-22 15:22:35 -0800

    Added basic state, will be helpful for styling.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,12 @@
+import { useState, useEffect } from 'react'
 
 const App = () => {
-
+  const [contacts, setContacts] = useState([
+    { id: 1, name: 'Valentinus Velo', number: '604-502-5123', favorite: true },
+    { id: 2, name: 'Eric Jandciu', number: '604-213-5621', favorite: true },
+    { id: 2, name: 'Greg Wolfgang', number: '778-203-4931', favorite: false },
+  ])
+  
   return (
     <div>
       <p>Hello</p>
