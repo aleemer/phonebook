8206f294ffd0a81d1b9f5fdacad16f332d3a3fda
aleemer <aleemertariq@gmail.com>
2024-02-23 07:43:40 -0800

    user setup.

---
+++ server/config/init_user.js
@@ -0,0 +1,3 @@
+/**
+ * Necessary imports and setup for connecting to our database and populating it with data
+ */
---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+// Person Schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  password: String
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
