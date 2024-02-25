190a011af8919f3e9c49339587f5a66217642149
aleemer <aleemertariq@gmail.com>
2024-02-24 22:00:07 -0800

    Fixed user initialization.

--- server/config/init_user.js
+++ server/config/init_user.js
@@ -4,16 +4,15 @@
 const { makeConnection, closeConnection } = require('./mongodb')
 const Person = require('../models/person')
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+
 let phonebook = 
 [
   { name: 'Valentius', number: '604-234-2195', favourite: true },
   { name: 'Phocas', number: '604-220-6792', favourite: false },
   { name: 'Darius', number: '604-778-2012', favourite: false }
 ]
-let users = 
-[
-  { username: 'aleem', password: 'special' }
-]
+
 
 /**
  * Make connection and add values internally, then close
@@ -23,6 +22,8 @@ const phonebookInsert = async () => {
 }
 
 const userInsert = async () => {
+  const genPassword = await bcrypt.hash('special', 10)
+  let users = [{ username: 'aleem', passwordHash: genPassword }]
   await User.insertMany(users)
 }
 
